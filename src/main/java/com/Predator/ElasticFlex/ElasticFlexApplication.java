fd971f4e87c216e929ceb48f70b67608dace355d
Shivang Saxena <shivangsaxena0596@gmail.com>
2022-04-24 03:14:40 +0530

    rev-commit

--- src/main/java/com/Predator/ElasticFlex/ElasticFlexApplication.java
+++ src/main/java/com/Predator/ElasticFlex/ElasticFlexApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElasticFlexApplication {
 
-	public static  main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Elastic flex running!!");
 		SpringApplication.run(ElasticFlexApplication.class, args);
 	}
